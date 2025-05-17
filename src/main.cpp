4b68bce41dbc611de995569c5ef7b2cc9d397b89
heesongkim23 <hkim163@go.pasadena.edu>
2025-05-16 23:55:59 -0700

    Draw a green circle using SFML

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
+
+
+    // 초록색 원 생성
+    sf::CircleShape circle(100.f); // 반지름 100
+    circle.setFillColor(sf::Color::Green); // 초록색
+
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -16,6 +22,10 @@ int main()
         }
 
         window.clear();
+        window.draw(circle);
+
         window.display();
     }
+
+    return 0;
 }
